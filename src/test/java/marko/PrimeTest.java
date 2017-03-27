1d21a256808e88847c25cc1c583f839f98e64242
MK <mrxkollo@users.noreply.github.com>
2017-03-27 12:34:38 +0300

    Rohkem teste

--- src/test/java/marko/PrimeTest.java
+++ src/test/java/marko/PrimeTest.java
@@ -8,15 +8,21 @@ public class PrimeTest {
 
     Rakendus r = new Rakendus();
 
-    /* answer = "Sisendatud arv ei ole algarv";
-       answer = "Sisendatud arv on algarv";*/
-
     @Test
     public void firstTest(){
 
-        assertEquals("Sisendatud arv on algarv", r.getPrime("5"));
-        assertEquals("Sisendatud arv on algarv", r.getPrime("10"));
-        assertEquals("Sisendatud arv on algarv", r.getPrime("hussa"));
+        //assertEquals("Sisendatud arv on algarv", r.getPrime("5"));	// Ei lähe läbi kuna tagastus peab olema "".
+        //assertEquals("Sisendatud arv on algarv", r.getPrime("10"));	// Ei lähe läbi, tagastys peaks olema "Sisendatud arv ei ole algarv".
+        //assertEquals("Sisendatud arv on algarv", r.getPrime("hussa"));// Ei lähe läbi, kuna tagastus peaks olema "Sisend ei ole arv".
+		//assertEquals("Sisendatud arv on algarv", r.getPrime("5.0"));	// Ei lähe läbi, kuna tagastus peaks olema "Sisend ei ole arv".
+		//assertEquals("Sisendatud arv on algarv", r.getPrime("123165656465468"));
+		
+		
+		assertEquals("", r.getPrime("5"));
+		assertEquals("Ei ole algarv", r.getPrime("16"));
+		assertEquals("Sisend ei ole arv", r.getPrime("hussa"));
+		assertEquals("Sisend ei ole arv", r.getPrime("123165656465468"));
+		
     }
 
 
