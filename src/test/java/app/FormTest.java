88a7e9ceab1d4001fda39b9ea8bd7a7c9bb404ca
root <root@ubuntu-512mb-fra1-01.localdomain>
2017-04-07 13:16:23 +0000

    final

--- src/test/java/app/FormTest.java
+++ src/test/java/app/FormTest.java
@@ -2,26 +2,31 @@ package app;
 
 import org.junit.*;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class FormTest{
 
+
+	@Autowired
+	private ManageTable manager;
+
 	@Test
-	public void test1(){
-		assertEquals(3, 2+1);
+	public void saveTest() {
+		Salary salary = new Salary("M", "R", -1);
+		manager.save(salary);
+		Assert.assertNotNull(manager.findOne(salary.id));
 	}
 	@Test
-	public void test2(){
-		Application a = new Application();
-		assertEquals("Salary saved!", a.create("", "", 000));
+	public void saveTest2(){
+		Salary salary = new Salary("", "", 1000);
+		manager.save(salary);
+		Assert.assertNotNull(manager.findOne(salary.id));
+		
 	}
-	@Test
-	public void test3(){
-		Application a = new Application();
-		assertEquals("Salary saved!", a.create("M", "R", -1));
-	}
-	@Test
-	public void test4(){
-		Application a = new Application();
-		assertEquals("Salary saved!", a.create("M", "R", 1000));
-	}
+
 }
