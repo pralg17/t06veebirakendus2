f47d71a76eb51cbd8896ddf4e246f5365cf436c9
melissab <melissab@greeny.cs.tlu.ee>
2017-05-09 14:56:13 +0300

    added

--- src/main/java/melissa/Rakendus.java
+++ src/main/java/melissa/Rakendus.java
@@ -10,16 +10,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 @SpringBootApplication
 public class Rakendus {
     @Autowired
-    private LiigiHaldur liigiHaldur;
+    private LiigiHaldur liigiHaldur1;
 
     @RequestMapping("/leia")
 	public String leia(String liik){
-	   Liik loom=liigiHaldur.findOne(liik);
+	   Liik loom=liigiHaldur1.findOne(liik);
 	   return "Looma "+liik+" nähti eelmine aasta looduses "+loom.nähtikorda+ " isendit.";
 	}
 
     public static void main(String[] args) {
-		System.getProperties().put("server.port", 5555);
+		System.getProperties().put("server.port", 4217);
+		System.getProperties().put("spring.datasource.url", "jdbc:mysql://localhost/if16_melissab?useSSL=false");
+		System.getProperties().put("spring.datasource.username", "if16");
+		System.getProperties().put("spring.datasource.password", "ifikad16");
         SpringApplication.run(Rakendus.class);
     }
 }
